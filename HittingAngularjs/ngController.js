32ed63c9ed587d8b262ec768f74ff30ee7a021ee
Admin-AIMS-PC <Admin-AIMS-PC@DESKTOP-445TFK1>
2016-12-25 14:45:17 +0200

    newLearning

---
+++ HittingAngularjs/ngController.js
@@ -0,0 +1,18 @@
+﻿var app = angular.module('myApp', []);
+app.directive('meInRed', function () {
+    return {
+        template: "<h1 style='color:red;'>Hello</h1>"
+    };
+})
+app.controller('myController', function ($scope) {
+    $scope.people = [{ name: 'Ahmed', age: 28 }, { name: 'Tawfeeq', age: 55 }, { name: 'Samya', age: 22 }];
+    $scope.getName = function () {
+        $scope.Name = {
+            first: $scope.fName, second: $scope.lName
+        }
+    };
+    $scope.local = "I'm local.";
+})
+app.controller('anotherCtrl', function ($rootScope) {
+    $rootScope.global = "I'm global";
+});
