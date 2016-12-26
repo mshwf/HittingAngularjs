fe14f8930405397694210db435665ae93b97af30
Admin-AIMS-PC <Admin-AIMS-PC@DESKTOP-445TFK1>
2016-12-26 19:26:36 +0200

    Select

--- HittingAngularjs/ngController.js
+++ HittingAngularjs/ngController.js
@@ -1,4 +1,33 @@
 ﻿var app = angular.module('myApp', []);
+app.controller('myController', function ($scope, $http, $timeout, $interval, mySvc) {
+
+    $scope.hex = mySvc.fun;
+    $scope.colors = { b: "blue", r: "red", g: "green", y: "yellow" };
+    $scope.people = [{ name: 'Ahmed', age: 28 }, { name: 'Tawfeeq', age: 55 }, { name: 'Samya', age: 22 }
+    , { name: 'Sohaib', age: 18 }, { name: 'Akram', age: 12 }, { name: 'Hamdy', age: 80 }, { name: 'Zeinab', age: 8 }];
+    $scope.names = ['Tareq', 'Ahmed', 'Masaod', 'Sobhy', 'Rehab', 'Lamiaa'];
+    $scope.getName = function () {
+        $scope.Name = {
+            first: $scope.fName, second: $scope.lName
+        }
+    };
+    $http.get('Testy.html').then(function (response) {
+        $scope.httpDone = response.data;
+        $scope.sayHello = "He never said it";
+        $timeout(function () {
+            $scope.sayHello = "Then he said Hello!";
+        }, 3000);
+        $interval(function () {
+            $scope.time = new Date().toLocaleTimeString()
+
+        }, 1000)
+    });
+    $scope.local = "I'm local.";
+    $scope.orderItBy = function (x) {
+        $scope.ord = x;
+    }
+
+});
 app.directive('meInRed', function () {
     return {
         template: "<h1 style='color:red;'>Hello</h1>"
@@ -19,43 +48,18 @@ app.filter('properName', function () {
         return newName;
     }
 });
-app.filter('toNum', function () {
+//Applying service in filter
+app.filter('toHexa', ['mySvc', function (mySvc) {
     return function (x) {
-        return !isNaN(x) ? x : parseInt(x);
-    }
-});
+        return mySvc.fun(x);
+    };
+}]);
 app.service('mySvc', function () {
     this.fun = function (s) {
         return s.toString(16);
     }
 })
-app.controller('myController', function ($scope, $http, $timeout, $interval, mySvc) {
-    $scope.people = [{ name: 'Ahmed', age: 28 }, { name: 'Tawfeeq', age: 55 }, { name: 'Samya', age: 22 }
-    , { name: 'Sohaib', age: 18 }, { name: 'Akram', age: 12 }, { name: 'Hamdy', age: 80 }, { name: 'Zeinab', age: 8 }];
-    $scope.names = ['Tareq', 'Ahmed', 'Masaod', 'Sobhy', 'Rehab', 'Lamiaa'];
-    $scope.getName = function () {
-        $scope.Name = {
-            first: $scope.fName, second: $scope.lName
-        }
-    };
-    $http.get('Testy.html').then(function (response) {
-        $scope.httpDone = response.data;
-        $scope.sayHello = "He never said it";
-        $timeout(function () {
-            $scope.sayHello = "Then he said Hello!";
-        }, 3000);
-        $interval(function () {
-            $scope.time = new Date().toLocaleTimeString()
 
-        }, 1000)
-        $scope.hex = mySvc.fun;
-    });
-    $scope.local = "I'm local.";
-    $scope.orderItBy = function (x) {
-        $scope.ord = x;
-    }
-
-});
 app.controller('anotherCtrl', function ($rootScope) {
     $rootScope.global = "I'm global";
 });
